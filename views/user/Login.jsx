ea0a31d7610d8268d67450044563c02cf1ce7dab
nguyen.danimal@gmail.com <endlesswaltz@MacBook-Pro-2.local>
2022-03-10 15:42:34 -0600

    css styling for Login.jsx

--- views/user/Login.jsx
+++ views/user/Login.jsx
@@ -5,18 +5,60 @@ const DefaultLayout = require('../Default.jsx');
 class Login extends React.Component {
     render() {
         return(
-            <DefaultLayout>
+            <html lang="en">
+                <head>
+                    <meta charset="UTF-8" />
+                    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"></link>
+                    <link rel="stylesheet" href="/css/main.css" />
+                    <link rel="stylesheet" href="/css/login.css" />
+                    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic"/>
+                    <title>LadyBug - Bug Tracker App</title>
+                </head>
+
+                <body>
+                <header>
+                <h1> LadyBug - A Bug Tracker App </h1>
+                    <div>
+                        <nav id="nav-btns">
+                            <ul class="nav justify-content-end">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="/bugs/"><button type="button" class="btn btn-outline-light">Home</button></a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="/bugs/New"><button type="button" class="btn btn-outline-light">New Ticket</button></a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="/user/logout"><button type="button" class="btn btn-outline-light">Logout</button></a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </header>
+
                 <div>
-                    <form action="/user/login" method="post">
+                    <form action="/user/login" method="POST">
                         <fieldset>
                             <legend>User Login</legend>
-                            <label>USERNAME: <input type="text" name="username" required /></label>
-                            <label>PASSWORD: <input type="password" name="password" required /></label>
-                            <input type="submit" value="Login" />
+                            <div class="form-floating mb-3">
+                                <input name="username" type="username" class="form-control" id="floatingInput" placeholder="Username" />
+                                <label for="floatingInput">Username</label>
+                            </div>
+                            <div class="form-floating">
+                                <input name="password" type="password" class="form-control" id="floatingPassword" placeholder="Password" />
+                                <label for="floatingPassword">Password</label>
+                            </div>
                         </fieldset>
+                        <input type="submit" value="Login" />
                     </form>
                 </div>
-            </DefaultLayout>
+
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+                
+                </body>
+
+            </html>
         )
     }
 };
--- views/user/Signup.jsx
+++ views/user/Signup.jsx
@@ -12,10 +12,29 @@ class Signup extends React.Component {
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"></link>
                 <link rel="stylesheet" href="/css/main.css" />
+                <link rel="stylesheet" href="/css/login.css" />
                 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic"/>
                 <title>LadyBug - Bug Tracker App</title>
             </head>
             <body>
+            <header>
+                <h1> LadyBug - A Bug Tracker App </h1>
+                    <div>
+                        <nav id="nav-btns">
+                            <ul class="nav justify-content-end">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="/bugs/"><button type="button" class="btn btn-outline-light">Home</button></a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="/bugs/New"><button type="button" class="btn btn-outline-light">New Ticket</button></a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="/user/logout"><button type="button" class="btn btn-outline-light">Logout</button></a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </header>
                 <div>
                     <form action="/user/signup" method="POST">
                         <fieldset>
