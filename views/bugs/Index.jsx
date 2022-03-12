26795dbd3fe6dc70abe32db1dc0804c235dcff96
nguyen.danimal@gmail.com <endlesswaltz@MacBook-Pro-2.local>
2022-03-12 11:15:32 -0600

    Bootstrap styling on Index and Show page

--- views/bugs/Index.jsx
+++ views/bugs/Index.jsx
@@ -5,26 +5,26 @@ class Index extends React.Component {
     render() {
         const { bugs } = this.props;
         return(
-            <html>
             <DefaultLayout>
                 <div class="card index-card">
-                <h2 id="home-text">Home: All Bugs</h2>
-                    {
-                        bugs.map((bug) => (
-                            <article>
-                                <ul class="list-group list-group-flush">
-                                    <li class="list-group-item">
-                                        <a href={`/bugs/${ bug._id }`}>
-                                            <h3>Bug Ticket: {bug.title}</h3>
-                                        </a>
-                                    </li>
-                                </ul>
-                            </article>
-                        ))
-                    }
+                    <div class="card-header">
+                        <h2 id="home-text">Home: All Bugs</h2>
+                    </div>
+                        {
+                            bugs.map((bug) => (
+                                <article>
+                                    <ul class="list-group list-group-flush">
+                                        <li class="list-group-item">
+                                            <a href={`/bugs/${ bug._id }`}>
+                                                <h3>Bug Ticket: {bug.title}</h3>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </article>
+                            ))
+                        }
                 </div>
             </DefaultLayout>
-            </html>
         )
     }
 };
--- views/bugs/Show.jsx
+++ views/bugs/Show.jsx
@@ -5,60 +5,31 @@ class Show extends React.Component {
     render() {
         const { bug } = this.props
         return(
-            <html lang="en">
-                <head>
-                    <meta charSet='UTF-8'/>
-                    <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                    <title>LadyBug - Bug Tracker App</title>
-                    <link rel="stylesheet" />
-                    <script
-                        src="https://code.jquery.com/jquery-3.6.0.min.js"
-                        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
-                        crossOrigin="anonymous">
-                    </script>
-                    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous"></link>
-
-                    <link rel="stylesheet" href="/css/main.css" />
-                    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic"/>
-                </head>
-                <body>
-                    <header>
-                        <h1> LadyBug - A Bug Tracker App </h1>
-                        <div>
-                            <nav id="nav-btns">
-                                <a href="/bugs/">Home</a>
-                                <a href="/bugs/New">New Ticket</a>
-                                <a href="/user/logout"><button>Logout</button></a>
-                            </nav>
-                        </div>
-                    </header>
-
+            <DefaultLayout>
                     <main>
-                        <div>
-                            <article>
-                                <div id="bugsList">
-                                    <h2 id="title">LadyBug: {bug.title}</h2>
-                                    <h2 id="category">Category: {bug.category}</h2>
-                                    <h2 id="status">Status: {bug.ticketStatus}</h2>
-                                    <h2 id="description">Description: {bug.description}</h2>
-                                    <h2 id="priority">Priority: {bug.priority}</h2>
-                                </div>
-                                <div id="nav2-btns">
-                                    <a href={`/bugs/${ bug._id }/edit`}><button>Edit</button></a>
-                                    <form action={`/bugs/${ bug._id }?_method=DELETE`} method="POST">
-                                        <input type="submit" value="Delete"/></form>
-                                    <a href="/bugs/"><button>Back to Main Index</button></a>
-                                </div>
-                            </article>
+                        <div class="card index-card">
+                            <div class="card-header">
+                                <h2 id="title">LadyBug: {bug.title}</h2>
+                            </div>
+                                <article>
+                                    <div id="bugsList" class="list-group list-group-flush">
+                                        <li class="list-group-item">
+                                            <h2 id="category">Category: {bug.category}</h2>
+                                            <h2 id="status">Status: {bug.ticketStatus}</h2>
+                                            <h2 id="description">Description: {bug.description}</h2>
+                                            <h2 id="priority">Priority: {bug.priority}</h2>
+                                        </li>
+                                    </div>
+                                </article>
+                        </div>
+                        <div id="editBtn" class="btn-group" role="group">
+                            <a href={`/bugs/${ bug._id }/edit`}><button type="button" class="btn btn-outline-primary">Edit</button></a>
+                            <form action={`/bugs/${ bug._id }?_method=DELETE`} method="POST">
+                                <input type="submit" class="btn btn-outline-primary"value="Delete"/></form>
                         </div>
                     </main>
 
-                    <footer>
-                    </footer>
-
-                </body>
-            </html>
+            </DefaultLayout>
         )
     }
 };
